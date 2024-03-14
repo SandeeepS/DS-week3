f6a013fff8c4a08a55128d98cf2b9aea3d02dd6a
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-14 11:33:56 +0530

    added delete method in max heap

--- heap/heap.js
+++ heap/heap.js
@@ -37,6 +37,26 @@ class MaxHeap{
         this.heapifyUp();
     }
 
+    //removing elements from a heap
+    delete(){
+        var item = this.arr.shift();
+        this.arr.unshift(this.arr.pop());
+        var index = 0 ;
+        var leftChild = this.getLeftChildIndex(index);
+        var rightChild = this.getRightChildIndex(index);
+        while(this.arr[leftChild]  && this.arr[leftChild] > this.arr[index] || this.arr[rightChild] > this.arr[index]){
+             var max = leftChild;
+             if(this.arr[rightChild] && this.arr[rightChild] > this.arr[max]){
+                max = rightChild
+             }
+             this.swap(max,index);
+             index = max;
+             leftChild = this.getLeftChildIndex(max);
+             rightChild =  this.getRightChildIndex(max);
+        }
+        return item;
+    }
+
     print(){
         console.log(this.arr);
     }
@@ -53,6 +73,8 @@ heap.push(50);
 heap.print();
 heap.push(65);
 heap.push(200);
+heap.print();
+heap.delete();
 heap.print();
 
 
