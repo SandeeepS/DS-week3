fee922e7b1677e3852aff086ec877cee2146d519
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-15 18:51:49 +0530

    implimented bfs and dfs in adct list

--- graph/adjacencyList.js
+++ graph/adjacencyList.js
@@ -22,4 +22,56 @@ class Graph{
     isEdge(fromNode,toNode){
         return this.list[fromNode].includes(toNode);
     }
+
+    bfs(startNode){
+        const visited = {};
+        const queue = [startNode];
+        visited[startNode] = true;
+
+        while(queue.length > 0){
+            const currentNode = queue.shift();
+            console.log(currentNode);
+
+            for(const neighbor of this.list[currentNode]){
+                if(!visited[neighbor]){
+                    queue.push(neighbor);
+                    visited[neighbor] = true
+                }
+            }
+        }
+    }
+
+    dfs(startNode) {
+        const visited = {};
+        this.dfsUtil(startNode, visited);
+    }
+
+    dfsUtil(node, visited) {
+        visited[node] = true;
+        console.log(node);
+
+        for (const neighbor of this.list[node]) {
+            if (!visited[neighbor]) {
+                this.dfsUtil(neighbor, visited);
+            }
+        }
+    }
 }
+
+let graph = new Graph();
+graph.addNode(5);
+graph.addNode(10);
+graph.addNode(25);
+graph.addNode(30);
+graph.addNode(35);
+
+graph.addEdge(5,10);
+graph.addEdge(10,25);
+graph.addEdge(25,10);
+graph.addEdge(25,30);
+graph.addEdge(30,35);
+
+
+graph.bfs(5);
+console.log("---------------------");
+graph.dfs(5)
