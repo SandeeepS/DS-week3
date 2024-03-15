44326ecfa56e12b1ac80502b5f1a2acf4f681848
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-15 18:41:39 +0530

    updated adjacency matrix

--- graph/adjacencyMatrix.js
+++ graph/adjacencyMatrix.js
@@ -21,4 +21,50 @@ class Graph{
     isEdge(fromNode,toNode){
         return this.matrix[fromNode][toNode] === 1;
     }
+
+    bfs(startNode){
+        const visited = new Array(this.matrix.length).fill(false);
+        const queue = [startNode];
+        visited[startNode] = true;
+
+        while(queue.length > 0){
+            const currentNode = queue.shift();
+            console.log(currentNode);
+
+            for(let i = 0 ; i < this.matrix.length; i++){
+                if(this.matrix[currentNode][i] === 1 && !visited[i]){
+                    queue.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    dfs(startNode){
+        const visited = new Array(this.matrix.length).fill(false);
+        this.dfsUtil(startNode,visited);
+    }
+
+    dfsUtil(node,visited){
+        visited[node] = true;
+        console.log(node);
+
+        for(let i = 0 ; i < this.matrix.length; i++){
+            if(this.matrix[node][i] === 1 && !visited[i]){
+                this.dfsUtil(i,visited);
+            }
+        }
+    }
+
+    
 }
+
+const graph = new Graph(5);
+graph.addEdge(0,1);
+graph.addEdge(1,2);
+graph.addEdge(2,3);
+graph.addEdge(3,4);
+
+graph.bfs(3);
+console.log("-----------------------------");
+graph.dfs(3)
