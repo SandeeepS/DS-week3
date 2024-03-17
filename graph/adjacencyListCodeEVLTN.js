81b7087da1b28f6cc9bbff4debf4214f2ef5665a
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-17 16:14:42 +0530

    added a another code to impliment adjacecny list by code evolution

---
+++ graph/adjacencyListCodeEVLTN.js
@@ -0,0 +1,45 @@
+//implimentaion of graph using adjacency list by code evolution
+
+class Graph{
+    constructor(){
+        this.list = {}
+    }
+
+    addVertx(vertx){
+        if(!this.list[vertx]){
+            this.list[vertx] = new Set();
+        }
+    }
+
+    addEdge(vertx1, vertx2){
+        if(!this.list[vertx1]){
+            this.addVertx(vertx1);
+        }
+
+        if(!this.list[vertx2]){
+            this.addVertx(vertx2);
+        }
+
+        this.list[vertx1].add(vertx2);
+        this.list[vertx2].add(vertx1);
+
+    }
+
+    display(){
+        for(let vertx in this.list){
+            console.log( vertx + " -> " + [...this.list[vertx]]);
+        }
+    }
+
+
+}
+
+let graph = new Graph();
+
+graph.addVertx("A");
+graph.addVertx("B");
+graph.addVertx("C");
+
+graph.addEdge("A","B");
+graph.addEdge("B","C");
+graph.display()
